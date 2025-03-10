2d60db30280c8c0be8da6f087779397a363ee740
ArielFlowers <arielmakotoflores@gmail.com>
2025-03-10 19:15:12 -0400

    feat: interfaz gráfica inicial

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,13 @@
+import { calcularPrecioFinal } from "./precio";
+
 document.getElementById("calcular").addEventListener("click", function() {
   const cantidad = parseInt(document.getElementById("cantidad").value);
   const precio = parseFloat(document.getElementById("precio").value);
   const estado = document.getElementById("estado").value;
 
   const precioFinal = calcularPrecioFinal(cantidad, precio, estado);
+
+  console.log(precioFinal); // Verifica que el cálculo sea correcto
+
   document.getElementById("resultado").innerText = `Precio total: $${precioFinal.toFixed(2)}`;
 });
